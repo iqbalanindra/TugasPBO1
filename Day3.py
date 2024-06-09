3dce67ff31ff00aff4b653a3ab0cec095fa3bb38
iqbalanindra <iqbalindra001@gmail.com>
2024-06-09 20:12:09 +0700

    Tugas PBO 30 Hari

---
+++ Day3.py
@@ -0,0 +1,5 @@
+jarak_km = float(input("masukkan jarak dalam kilometer"))
+
+jarak_meter = int(jarak_km * 1000)
+
+print(f"jarak dalam meter: {jarak_meter} meter")
--- Tugas 1.py
+++
@@ -1,5 +0,0 @@
-jarak_km = float(input("masukkan jarak dalam kilometer"))
-
-jarak_meter = int(jarak_km * 1000)
-
-print(f"jarak dalam meter: {jarak_meter} meter")
