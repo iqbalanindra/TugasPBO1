8d5e0ceb4919179d60b4e369828e18b820765e2c
iqbalanindra <iqbalindra001@gmail.com>
2024-06-10 07:46:25 +0700

    Add files via upload

---
+++ Tugas 1.py
@@ -0,0 +1,12 @@
+kata = 'Pendidikan Teknik Informatika'
+nama_depan = input('Nama depan :')
+nama_belakang = input('Nama belakang :')
+nim = input('Nim :')
+fakultas = 'Fakultas Pendidikan dan Ilmu Pendidikan'
+kampus = 'universitas muhammdiyah surakarta'
+print(kata.center(50))
+print('Nama: {} {}'.format(nama_depan, nama_belakang))
+print('Nim: ', nim)
+print(fakultas)
+kapital = kampus.upper()
+print(kapital.center(50))
