52aaaa54ee11c2d2bdf6208b33d240dc3f221163
iqbalanindra <iqbalindra001@gmail.com>
2024-06-13 23:07:01 +0700

    Tugas PBO 7,30 Hari

--- Tugas 2.py
+++
@@ -1,9 +0,0 @@
- # Input jumlah anak bebek
-jumlah_anak_bebek = int(input("Masukkan jumlah anak bebek: "))
-
-# Loop untuk menampilkan lirik lagu
-for i in range(jumlah_anak_bebek, 0, -1):
-    if i > 1:
-        print(f"Anak bebek turun {i}, mati satu tinggal {i - 1}")
-    else:
-        print("Anak bebek turun 1, mati satu tinggal induknya")
